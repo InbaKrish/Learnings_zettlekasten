353955f9442313dd50cde2b64b25aeb5b6a16f36
InbaKrish <inbakrish007@gmail.com>
2025-01-20 21:44:45 +0530

    vault backup: 2025-01-20 21:44:45

--- Leetcode_Probs/product_except_self.rb
+++ Leetcode_Probs/product_except_self.rb
@@ -3,12 +3,16 @@ def product_except_self(nums)
 end
 
 def pes1bf(nums)
-    (0..nums.size).each do |i|
+    result = []
+    (0..nums.size - 1).each do |i|
         prefix = i.zero? ? [] : nums.slice(..i-1)
         suffix = nums.slice(i+1..)
-        p prefix
-        p suffix
+        cur_r = 1
+        prefix.each { |k| cur_r *= k }
+        suffix.each { |k| cur_r *= k }
+        result << cur_r
     end
+    result
 end
 
 nums = [1,2,3,4]
